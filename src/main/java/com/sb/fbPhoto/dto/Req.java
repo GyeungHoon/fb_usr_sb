c8b091faba413e75f058cf6df7e1a378e7b8b45f
GyeungHoon <ghp0415@naver.com>
2023-06-27 17:45:08 +0900

    Rq를 이용해서 head에 로그인 상태에 따라 메뉴 분기

---
+++ src/main/java/com/sb/fbPhoto/dto/Req.java
@@ -0,0 +1,29 @@
+package com.sb.fbPhoto.dto;
+
+public class Req {
+	private Member loginedMember;
+	
+	public Req(Member loginedMember) {
+		this.loginedMember = loginedMember;
+	}
+	
+	public boolean isLogined() {
+		return loginedMember != null;
+	}
+	public boolean isNotLogined() {
+		return isLogined() == false;
+	}
+	
+	public int getLoginedMemberId() {
+		if(isNotLogined()) return 0;
+		
+		return loginedMember.getId();
+	}
+	public Member getLoginedMember() {
+		return loginedMember;
+	}
+	public String getLoginedMemberNickname() {
+		if(isNotLogined()) return "";
+		return loginedMember.getNickname();
+	}
+}
